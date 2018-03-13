90b56846033002dc0574efd7e44fd13657635f6b
Sergey Sherkunov <leinlawun@leinlawun.org>
2018-03-14 02:08:16 +0300

    Added new example

--- examples/hello.rs
+++ examples/hello.rs
@@ -10,6 +10,7 @@
 #![plugin(sig)]
 #![sig]
 
+use std::cell::RefCell;
 use std::rc::Rc;
 
 macro_rules! connect {
@@ -18,7 +19,9 @@ macro_rules! connect {
     }
 }
 
-struct Hello {}
+struct Hello {
+    value: u32,
+}
 
 impl Hello {
     #[sig]
@@ -33,6 +36,14 @@ impl Hello {
 
     pub fn hello_galaxy(&self) {
         println!("Galaxy!");
+    }
+
+    pub fn answer(&mut self) {
+        println!("answer before: {}", self.value);
+
+        self.value += 5;
+
+        println!("answer after: {}", self.value);
     }
 }
 
@@ -52,14 +63,22 @@ fn main() {
     let mut hello1 = Hello {
         hello: vec![],
         hello_person: vec![],
+        value: 0,
     };
-    let hello2 = Hello {
+    let mut hello2 = Hello {
         hello: vec![],
         hello_person: vec![],
+        value: 0,
     };
+    let hello3 = Rc::new(RefCell::new(Hello {
+        hello: vec![],
+        hello_person: vec![],
+        value: 0,
+    }));
 
     connect!(hello1, hello, world);
     connect!(hello1, hello, move || hello2.hello_galaxy());
+    connect!(hello1, hello, move || hello3.borrow_mut().answer());
     connect!(hello1, hello, universe);
     connect!(hello1, hello_person, person);
 
